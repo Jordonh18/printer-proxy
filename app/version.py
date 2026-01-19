0615096582596ee121e7599d4ef23d77be7ca1d1
Jordon Harrison <Jordon.Harrison@outlook.com>
2026-01-19 10:52:16 +0000

    Bump version to 1.0.0-beta.2

--- app/version.py
+++ app/version.py
@@ -5,7 +5,7 @@ Update this file to change the version everywhere.
 The build script and templates read from here.
 """
 
-__version__ = "1.0.0-beta.1"
+__version__ = "1.0.0-beta.2"
 __version_info__ = (1, 0, 0)
 
 # For display
